093118005a917f8bd64785d14b009064dec601ca
Russell Brown <russell.brown@joyent.com>
2019-05-24 11:28:36 +0100

    Muppet config seems to allow ipv6 addresses
    Change the type to the looser enum type IpAddr as config allows ipv6
    addresses (afaict)

--- src/config.rs
+++ src/config.rs
@@ -2,11 +2,10 @@
  * Copyright (c) 2019, Joyent, Inc.
  */
 
-
 use std::error::Error;
 use std::fs::File;
 use std::io::BufReader;
-use std::net::Ipv4Addr;
+use std::net::IpAddr;
 use std::path::Path;
 
 use serde_derive::{Deserialize, Serialize};
@@ -18,23 +17,23 @@ struct MantaDomain(pub String);
 #[allow(non_snake_case)]
 pub struct Config {
     name: MantaDomain,
-    trustedIP: Ipv4Addr,
-    adminIPs: Option<Vec<Ipv4Addr>>,
-    mantaIPs: Option<Vec<Ipv4Addr>>,
-    untrustedIPs: Option<Vec<Ipv4Addr>>,
-    zookeeper: ZookeeperConfig
+    trustedIP: IpAddr,
+    adminIPs: Option<Vec<IpAddr>>,
+    mantaIPs: Option<Vec<IpAddr>>,
+    untrustedIPs: Option<Vec<IpAddr>>,
+    zookeeper: ZookeeperConfig,
 }
 
 #[derive(Serialize, Deserialize)]
 pub struct ZookeeperConfig {
     servers: Vec<ZookeeperServer>,
-    timeout: u64
+    timeout: u64,
 }
 
 #[derive(Serialize, Deserialize)]
 pub struct ZookeeperServer {
     host: String,
-    port: u32
+    port: u32,
 }
 
 pub fn read_file<P: AsRef<Path>>(path: P) -> Result<Config, Box<Error>> {
